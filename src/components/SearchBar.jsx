85415f82ac912b9e579acb62d32c9bb607236a08
Bogdan <bogdankuzyk14@gmail.com>
2024-02-27 14:13:06 +0200

    Create the Searchbar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,25 @@
 import "./SearchBar.css"
 
 function SearchBar() {
-	return <div>SearchBar</div>
+	return (
+		<div className="search-bar">
+			<form
+				action="#"
+				className="search-form d-flex align-items-center"
+				method="POST"
+			>
+				<input
+					type="text"
+					name="query"
+					placeholder="Search"
+					title="Enter search keyword"
+				/>
+				<button type="submit" title="Search">
+					<i className="bi bi-search"></i>
+				</button>
+			</form>
+		</div>
+	)
 }
 
 export default SearchBar
